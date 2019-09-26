820d5cd839ebed33b05c37989969b102c87b305f
Ubuntu <dbelinchonal18dw@ikzubirimanteo.com>
2019-09-26 09:32:30 +0200

    Actualizo ejercicio 1

--- ej1.php
+++ ej1.php
@@ -5,7 +5,18 @@
  <body>
  <?php 
  
- echo '<p>Hola Mundo</p>'; 
+
+ $piso = 3;
+ $puerta = 4;
+ $letras = ["A","B","C","D","E","F","G","H"];
+ for($i = 1; $i <= $piso; $i++){
+     for($j=0; $j <=$puerta; $j++){
+         echo "$i"."."." Puerta $letras[$j]"."\n";
+     }
+     echo "<br>";
+ }
+
+
  
  ?>
  </body>
