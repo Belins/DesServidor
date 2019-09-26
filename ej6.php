19e43d17baa0539ad851a7c9ffd979505fd8ac33
Ubuntu <dbelinchonal18dw@ikzubirimanteo.com>
2019-09-26 09:33:32 +0200

    Subo ejercicio 6

---
+++ ej6.php
@@ -0,0 +1,17 @@
+<html>
+ <head>
+  <title>Potencias</title>
+ </head>
+ <body>
+ <?php
+
+$a = 1;
+do {
+    $b = (pow($a, 3));
+    echo $a. " -- ".$b."<br>";
+    $a ++;
+}
+while($a <=4);
+ ?>
+ </body>
+</html>
