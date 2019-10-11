6bf7b41074e5c9bcb30a0edbd51d9cc616129275
David Belinchon <dbelinchon@DOMUKABI.local>
2019-10-11 17:31:14 +0200

    Corrijo para no perder nombre en titulo agenda

---
+++ Agenda/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+	<head>
+		<title>
+			Agenda
+		</title>
+	</head>
+	<body>
+		<?php
+			
+		?>
+		<!-- Indicamos que las acciones se puedan recojer en agenda2.php mediante el metodo POST -->
+		<form action="agenda2.php" method="post">
+			Nombre:<br>
+			<input type="text" name="nomb"><br><br>
+            <input type="Submit" value="Enviar">
+        </form>
+	</body>
+</html>
